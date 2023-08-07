631f3114cfbb1b93c1032b2b9d3f430ebe2c2606
b0xitron <100628630+b0xitron@users.noreply.github.com>
2023-08-07 20:16:12 +0200

    enemy controller changes

--- Saberfall/Assets/EnemyBehavior.cs
+++ Saberfall/Assets/EnemyBehavior.cs
@@ -6,7 +6,7 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private int damageAmount = 10;
 
-    [SerializeField] LayerMask groundLayer; // This is used to detect the ground.
+    [SerializeField] LayerMask groundLayer; //make sure layer is assigned for enemy
     private bool isGrounded;
 
     private bool movingRight = true;
@@ -23,6 +23,7 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] private float attackRange = 2f;
     [SerializeField] private int attackDamage = 20;
     [SerializeField] private float attackCooldown = 1f;
+    [SerializeField] private float patrolRadius = 5f;
     private float timeSinceLastAttack = 0;
     private Transform player;
     public PatrolPath path;
@@ -36,6 +37,7 @@ public class EnemyBehavior : MonoBehaviour
     private const string RUN = "void_run";
     private const string DEATH = "void_die";
     private const string ATTACK = "void_attack";
+    private const string HURT = "void_hurt";
 
     private void Start()
     {
@@ -58,13 +60,15 @@ public class EnemyBehavior : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
 
-        ChangeAnimationState(IDLE); //todo initaliaze or replace with tag?
+        //anim.SetTrigger("enemyIdle");
+        //ChangeAnimationState(IDLE); //todo initaliaze or replace with tag?
 
         //float widthScaleFactor = 202f / 37f;
         //float heightScaleFactor = 123f / 27f;
         //transform.localScale = new Vector3(widthScaleFactor, heightScaleFactor, 1f);
 
     }
+
 
     private void Update()
     {
@@ -79,10 +83,11 @@ public class EnemyBehavior : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, 0);
         }
 
-        if (player != null)
-        {
-            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
+        // Check if player is within the patrol radius
+        if (distanceToPlayer <= patrolRadius)
+        {
             if (distanceToPlayer < attackRange)
             {
                 if (timeSinceLastAttack > attackCooldown)
@@ -91,104 +96,40 @@ public class EnemyBehavior : MonoBehaviour
                     timeSinceLastAttack = 0;
                 }
             }
-        }
-        else
-        {
-            // Here's the patrol logic:
-            if (mover != null)
+            else if (mover != null) // Patrol logic
             {
                 Vector2 newPosition = mover.Position;
                 movingRight = newPosition.x > transform.position.x;
                 transform.position = newPosition;
 
                 sprite.flipX = !movingRight; // flip the enemy sprite based on direction
-                ChangeAnimationState(RUN);
+
+                anim.SetTrigger("enemyRun");
+                // ChangeAnimationState(RUN);
             }
+        }
+        else if (mover != null) // If the player is not within the patrol radius, continue patrolling.
+        {
+            Vector2 newPosition = mover.Position;
+            movingRight = newPosition.x > transform.position.x;
+            transform.position = newPosition;
+
+            sprite.flipX = !movingRight; // flip the enemy sprite based on direction
+
+            anim.SetTrigger("enemyRun");
+            // ChangeAnimationState(RUN);
         }
         timeSinceLastAttack += Time.deltaTime;
     }
 
 
-    //private void Update()
-    //{
-    //    isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundLayer);
-
-    //    // If the enemy is not grounded, apply a gravity-like force.
-    //    if (!isGrounded)
-    //    {
-    //        rb.velocity = new Vector2(rb.velocity.x, -2);  
-    //    }
-    //    else
-    //    {
-    //        rb.velocity = new Vector2(rb.velocity.x, 0);  
-    //    }
-
-    //    //if (path != null)
-    //    //{
-    //    //    if (mover == null) mover = path.CreateMover(control.maxSpeed * 0.5f);
-    //    //    control.move.x = Mathf.Clamp(mover.Position.x - transform.position.x, -1, 1);
-    //    //}
-    //    // movement
-    //    if (movingRight)
-    //    {
-    //        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-    //        sprite.flipX = false;
-    //        ChangeAnimationState(RUN);
-    //    }
-    //    else
-    //    {
-    //        rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
-    //        sprite.flipX = true;
-    //        ChangeAnimationState(RUN);
-    //    }
-
-    //    //if (groundedController2D != null)
-    //    //{
-    //    //    groundedController2D.CheckCapsuleEndCollisions();
-    //    //}
-    //    //else
-    //    //{
-    //    //    Debug.LogWarning("groundedController2D is not initialized!");
-    //    //}
-
-    //    // Check for zero health
-    //    if (_enemyHealth.Health <= 0)
-    //    {
-    //        ChangeAnimationState(DEATH);
-    //        Destroy(gameObject, 1f);
-    //    }
-
-    //    // Attack logic
-    //    //float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-    //    //if (distanceToPlayer < attackRange && timeSinceLastAttack > attackCooldown)
-    //    //{
-    //    //    Attack();
-    //    //    timeSinceLastAttack = 0;
-    //    //}
-    //    //timeSinceLastAttack += Time.deltaTime;
-
-    //    if (player != null) //todo add better error handling
-    //    {
-    //        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
-    //        if (distanceToPlayer < attackRange)
-    //        {
-    //            if (timeSinceLastAttack > attackCooldown)
-    //            {
-    //                Attack();
-    //                timeSinceLastAttack = 0;
-    //            }
-    //        }
-    //        timeSinceLastAttack += Time.deltaTime;
-    //    }
-    //}
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if (collision.gameObject.CompareTag("Player"))
-        //{
+        if (collision.gameObject.CompareTag("Player"))
+        {
 
-        //    GameManager.gameManager._playerHealth.DamageUnit(damageAmount, defaultKnockback);
-        //}
+            GameManager.gameManager._playerHealth.DamageUnit(damageAmount, defaultKnockback);
+        }
         //else if (collision.gameObject.CompareTag("Wall"))
         //{
 
@@ -211,8 +152,10 @@ public class EnemyBehavior : MonoBehaviour
 
     private void Attack()
     {
-        ChangeAnimationState(ATTACK);
 
+        //ChangeAnimationState(ATTACK);
+
+        anim.SetTrigger("enemyAttack");
         // Check if player is in range
         //Collider2D[] hitPlayers = Physics2D.OverlapCircleAll(transform.position, attackRange);
         //foreach (Collider2D player in hitPlayers)
