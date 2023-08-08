23f40e091eed9c89da8400dd98b926183c6cbd99
Yaoqi dong <98054482+yaoqidong@users.noreply.github.com>
2023-08-07 20:03:27 -0400

    the script

--- Saberfall/Assets/Script/FinishCheckpoint.cs
+++ Saberfall/Assets/Script/FinishCheckpoint.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishCheckpoint : MonoBehaviour
 {
+    //private BoxCollider2D coll;
     private AudioSource finishSound;
     // Start is called before the first frame update
     private void Start()
@@ -11,19 +13,21 @@ public class FinishCheckpoint : MonoBehaviour
         finishSound = GetComponent<AudioSource>;
     }
 
-    private void OnTriggerEnter2D(collider2D collision)
+
+    private void OnTriggerEnter2D(collider collision)
     {
         if(collision.gameObject.name == "Player")
         {
             finishSound.Play();
+            Invoke("CompleteLevel", 2f);
             CompleteLevel();
         }
     }
 
     private void CompleteLevel()
     {
+        SceneManagement.LoadScene(SceneManagement.GetActivityScene().buildIndex + 1);
 
     }
-
     
 }
